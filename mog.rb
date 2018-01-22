4ff21e26cfa101da05b028a91b49be26746f675d
Shota Suzuki <aqr.aqua@gmail.com>
2018-01-22 23:32:38 +0900

    Bump version to v0.1.3

--- mog.rb
+++ mog.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Mog < Formula
   homepage "https://github.com/szyn/mog"
-  version 'v0.1.2'
+  version 'v0.1.3'
 
   url "https://github.com/szyn/mog/releases/download/#{version}/mog_darwin_amd64.tar.gz"
-  sha256 "653a444a949c41d31df5568f8347d683c4460445d8a8b19776240c385f0e55a9"
+  sha256 "27f316bd15c83bb2a398e08944fe6910c393fc5a3de6c9a82fd3f7492a27ed16"
 
   def install
     bin.install 'mog'
