7948e6bba9fff3373ee346f89f02f73f3bd5d7a4
Shota Suzuki <aqr.aqua@gmail.com>
2018-02-13 09:52:08 +0900

    Update mog.rb

--- mog.rb
+++ mog.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Mog < Formula
   homepage "https://github.com/szyn/mog"
-  version 'v0.1.4'
+  version 'v0.1.5'
 
   url "https://github.com/szyn/mog/releases/download/#{version}/mog_darwin_amd64.tar.gz"
-  sha256 "d660eb870a11fa593a320b0bf8e697104b6c711c47b042dd4e9c43d0ed99abe8"
+  sha256 "5ba5f66714f998b8f8d2ff65092c2c621dfd58f2598add26a37c68323a6b7c3d"
 
   def install
     bin.install 'mog'
